44e06001508c19d47673af4b837aaad8a06c1a86
Nugraha Adi Sulistyo <tyoku1792@gmail.com>
2022-03-09 20:11:36 +0700

    IndexController Completed + Param

--- src/main/java/com/nugrahaas/demo/IndexController.java
+++ src/main/java/com/nugrahaas/demo/IndexController.java
@@ -2,14 +2,14 @@ package com.nugrahaas.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api")
 public class IndexController {
     @GetMapping("home")
-    public String home(String nama){
-        nama = "Nugraha";
-        return "Halo, selamat datang " + nama;
+    public String home(@RequestParam String nama){
+        return "Halo, selamat datang " + nama + " ";
     }
 }
